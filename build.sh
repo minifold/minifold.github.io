63f9e583eba40c935e6509f73b2200178437aaca
A. Cingoranelli <49851131+minifold@users.noreply.github.com>
2023-06-04 22:52:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "Running build script..."
 mkdir -p content/images/generated
-emacs --batch --no-init-file --load publish.el content/*/*.org --funcall npl-publish-all
+emacs --batch --no-init-file --load publish.el content/test.org --funcall npl-publish-all
